62dea5c0a347a4912fc4875cbbbb9bcbf1c2680e
thards89 <thards89@gmail.com>
2023-02-01 21:38:21 +0100

    db updated

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const app = express();
 // CORS middleware:  * Since our api is hosted on a different domain than our client
 // we are are doing "Cross Origin Resource Sharing" (cors)
 // Cross origin resource sharing is disabled by express by default
-app.use(corsMiddleWare());
+app.use(corsMiddleWare({   
+  origin: "*",   
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",   
+  allowedHeaders:
+  "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token",   credentials: true,   preflightContinue: false,  
+  optionsSuccessStatus: 204 }));
 
 // express.json():be able to read request bodies of JSON requests a.k.a. body-parser
 const bodyParserMiddleWare = express.json();
